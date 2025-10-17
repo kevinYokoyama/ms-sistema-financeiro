49002619e1d8558d00680018ca4250f45a4abe26
kevinYokoyama <kevin.yokoyama@picpay.com>
2025-10-17 17:52:07 -0300

    espacement in the classes

--- src/main/java/com/example/FinancialSystem/core/domain/Installment.java
+++ src/main/java/com/example/FinancialSystem/core/domain/Installment.java
@@ -4,10 +4,14 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class Installment {
-    private String id;
-    private LocalDate dataVendicemnto;
-    private BigDecimal totalAmount;
-    private Contract contract;
-    private InstallmentStatus status;
 
+    private String id;
+
+    private LocalDate dataVendicemnto;
+
+    private BigDecimal totalAmount;
+
+    private Contract contract;
+
+    private InstallmentStatus status;
 }
--- src/main/java/com/example/FinancialSystem/core/domain/Payment.java
+++ src/main/java/com/example/FinancialSystem/core/domain/Payment.java
@@ -3,9 +3,14 @@ package com.example.FinancialSystem.core.domain;
 import java.time.LocalDate;
 
 public class Payment {
+
     private Double amoutPaid;
+
     private Double remainingAmount;
+
     private LocalDate datePayment;
+
     private PaymentMethod paymentMethod;
+
     private Installment installment;
 }
