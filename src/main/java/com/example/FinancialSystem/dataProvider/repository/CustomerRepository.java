6109d0e25ec40a7ee6042a73b3b5637263e0343d
kevinYokoyama <kevin.yokoyama@picpay.com>
2025-10-27 15:57:13 -0300

    fixed the application.yml and added the annotation @Repository in all interfaces in repository

--- src/main/java/com/example/FinancialSystem/dataProvider/repository/CustomerRepository.java
+++ src/main/java/com/example/FinancialSystem/dataProvider/repository/CustomerRepository.java
@@ -2,10 +2,11 @@ package com.example.FinancialSystem.dataProvider.repository;
 
 import com.example.FinancialSystem.dataProvider.entity.CustomerEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CustomerRepository extends MongoRepository<CustomerEntity, String> {
-    @Override
-    List<CustomerEntity> findAllById(Iterable<String> strings);
+
 }
--- src/main/java/com/example/FinancialSystem/dataProvider/repository/PaymentRepository.java
+++ src/main/java/com/example/FinancialSystem/dataProvider/repository/PaymentRepository.java
@@ -2,14 +2,11 @@ package com.example.FinancialSystem.dataProvider.repository;
 
 import com.example.FinancialSystem.dataProvider.entity.PaymentEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PaymentRepository extends MongoRepository <PaymentEntity, String> {
-    @Override
-    List<PaymentEntity> findAllById(Iterable<String> strings);
-
-    @Override
-    List<PaymentEntity> findAll();
 
 }
