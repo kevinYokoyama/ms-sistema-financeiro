90c616e06c52886519cdf7dee9b42d7e0e13d45c
kevinYokoyama <kevin.yokoyama@picpay.com>
2025-11-12 15:35:00 -0300

    added a new method in apiError and CustomGlobalExceptionHandler and added a new dependency

--- src/main/java/com/example/FinancialSystem/entryPoint/handler/ApiError.java
+++ src/main/java/com/example/FinancialSystem/entryPoint/handler/ApiError.java
@@ -1,18 +1,29 @@
 package com.example.FinancialSystem.entryPoint.handler;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiError {
 
     private String message;
 
     private String code;
 
+    private List<String> errors;
+
     public ApiError(String message, String code) {
         this.message=message;
         this.code=code;
+    }
+
+    public ApiError(List<String> errors, String message) {
+        this.errors=errors;
+        this.message=message;
     }
 }
