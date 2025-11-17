4279d40277c93903ee4246d429e6e403af7f7e86
kevinYokoyama <kevin.yokoyama@picpay.com>
2025-11-17 17:43:37 -0300

    added getter and setter in customerDto

--- src/main/java/com/example/FinancialSystem/entryPoint/dto/CustomerDto.java
+++ src/main/java/com/example/FinancialSystem/entryPoint/dto/CustomerDto.java
@@ -1,9 +1,13 @@
 package com.example.FinancialSystem.entryPoint.dto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
+@Setter
+@Getter
 public class CustomerDto {
 
     @NotNull(message = "Name cannot be null")
