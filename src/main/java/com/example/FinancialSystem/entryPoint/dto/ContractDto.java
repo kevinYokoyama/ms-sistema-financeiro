e642f8a91d32d2c9da46cd6f8ce2d10483597945
kevinYokoyama <kevin.yokoyama@picpay.com>
2025-12-01 17:47:32 -0300

    adding the annotation schema in all 3 DTOs

--- src/main/java/com/example/FinancialSystem/entryPoint/dto/ContractDto.java
+++ src/main/java/com/example/FinancialSystem/entryPoint/dto/ContractDto.java
@@ -1,5 +1,6 @@
 package com.example.FinancialSystem.entryPoint.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,11 +12,14 @@ import java.math.BigDecimal;
 public class ContractDto {
 
     @NotNull(message = "Request amount cannot be null")
+    @Schema(description = "Requested amount of the contract", example = "200.10")
     private BigDecimal requestAmount;
 
     @NotNull(message = "Operation period cannot be null")
+    @Schema(description = "Operation period is the total time the customer pays for service", example = "3")
     private Integer operationPeriod;
 
     @NotNull(message = "Customer id cannot be null")
+    @Schema(description = "Id of the customer", example = "abcdefghijklmnopqrstuvwxyz123")
     private String customerId;
 }
--- src/main/java/com/example/FinancialSystem/entryPoint/dto/PaymentDto.java
+++ src/main/java/com/example/FinancialSystem/entryPoint/dto/PaymentDto.java
@@ -1,5 +1,6 @@
 package com.example.FinancialSystem.entryPoint.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,11 +12,14 @@ import java.math.BigDecimal;
 public class PaymentDto {
 
     @NotNull(message = "Amount paid cannot be null")
+    @Schema(description = "Amount paid of the payment", example = "14991.41")
     private BigDecimal amountPaid;
 
     @NotNull(message = "Payment method cannot be null")
+    @Schema(description = "Payment method can be PIX, credit card or debit card", example = "CREDIT_CARD")
     private String paymentMethod;
 
     @NotNull(message = "Installment cannot be null")
+    @Schema(description = "Id of the installment paid", example = "0002")
     private String installmentId;
 }
