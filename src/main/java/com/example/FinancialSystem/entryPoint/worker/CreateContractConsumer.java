9bf96ae6a085de437be42e5d900d7eec11c95c78
kevinYokoyama <kevin.yokoyama@picpay.com>
2025-12-04 17:39:38 -0300

    created the create consumer of contract, customer and payment

---
+++ src/main/java/com/example/FinancialSystem/entryPoint/worker/CreateContractConsumer.java
@@ -0,0 +1,36 @@
+package com.example.FinancialSystem.entryPoint.worker;
+
+import com.example.FinancialSystem.core.exception.Contract.ContractOperationPeriodNotAllowed;
+import com.example.FinancialSystem.core.exception.Contract.ContractRequestAmountNotAllowedException;
+import com.example.FinancialSystem.core.useCase.ContractUseCase.CreateContractUseCase;
+import com.example.FinancialSystem.entryPoint.dto.ContractDto;
+import com.example.FinancialSystem.entryPoint.mapper.ContractMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
+
+import java.util.function.Consumer;
+
+@Slf4j
+@RequiredArgsConstructor
+public class CreateContractConsumer {
+
+    private final ContractMapper contractMapper;
+
+    private final CreateContractUseCase createContractUseCase;
+
+    @Bean
+    Consumer<Message<ContractDto>> createLoanEvent() {
+        return this::receive;
+    }
+
+    public void receive(Message<ContractDto> message) {
+        try {
+            var contract = contractMapper.toDomain(message.getPayload());
+            createContractUseCase.execute(contract);
+        } catch (ContractOperationPeriodNotAllowed | ContractRequestAmountNotAllowedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/com/example/FinancialSystem/entryPoint/worker/CreatePaymentConsumer.java
@@ -0,0 +1,36 @@
+package com.example.FinancialSystem.entryPoint.worker;
+
+import com.example.FinancialSystem.core.exception.Payment.PaymentAmountPaidNotAllowdException;
+import com.example.FinancialSystem.core.exception.Payment.PaymentMethodNotFoundException;
+import com.example.FinancialSystem.core.useCase.PaymentUseCase.CreatePaymentUseCase;
+import com.example.FinancialSystem.entryPoint.dto.PaymentDto;
+import com.example.FinancialSystem.entryPoint.mapper.PaymentMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
+
+import java.util.function.Consumer;
+
+@Slf4j
+@RequiredArgsConstructor
+public class CreatePaymentConsumer {
+
+    private final PaymentMapper paymentMapper;
+
+    private final CreatePaymentUseCase createPaymentUseCase;
+
+    @Bean
+    Consumer<Message<PaymentDto>> createLoanEvent() {
+        return this::receive;
+    }
+
+    public void receive(Message<PaymentDto> message) {
+        try {
+            var customer = paymentMapper.toDomain(message.getPayload());
+            createPaymentUseCase.execute(customer);
+        } catch (PaymentMethodNotFoundException | PaymentAmountPaidNotAllowdException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
