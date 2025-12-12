fa11530d81cc56bff3cb1f9ea3e297f81b900597
kevinYokoyama <kevin.yokoyama@picpay.com>
2025-12-12 11:58:04 -0300

    addapting the zip code

--- src/main/java/com/example/FinancialSystem/core/useCase/CustomerUseCase/CreateCustomerUseCase.java
+++ src/main/java/com/example/FinancialSystem/core/useCase/CustomerUseCase/CreateCustomerUseCase.java
@@ -3,6 +3,7 @@ package com.example.FinancialSystem.core.useCase.CustomerUseCase;
 import com.example.FinancialSystem.core.domain.Customer;
 import com.example.FinancialSystem.core.domain.enumeration.CustomerStatus;
 import com.example.FinancialSystem.core.exception.Customer.CustomerBirthdateNotAllowed;
+import com.example.FinancialSystem.core.exception.Customer.CustomerZipCodeNotFoundException;
 import com.example.FinancialSystem.core.gateway.GenericGateway;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,11 +16,16 @@ public class CreateCustomerUseCase {
 
     private final GenericGateway<Customer> customerGateway;
 
-    public Customer execute(Customer customer) throws CustomerBirthdateNotAllowed {
+    public Customer execute(Customer customer) throws CustomerBirthdateNotAllowed, CustomerZipCodeNotFoundException {
 
         if (customer.getBirthdate().getYear() < 18) {
             log.error("Customer must be more than eighteen years old");
             throw new CustomerBirthdateNotAllowed();
+        }
+
+        if (customer.getZipcode() == null) {
+            log.error("Zipcode not found");
+            throw new CustomerZipCodeNotFoundException(customer.getZipcode());
         }
 
         customer.setName(customer.getName());
