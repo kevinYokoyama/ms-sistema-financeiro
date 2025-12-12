2446e79efba4646515f31d1e27d4ea7098d0d581
kevinYokoyama <kevin.yokoyama@picpay.com>
2025-12-12 11:49:49 -0300

    creating the adapter, client and the response of the ViaCep API

---
+++ src/main/java/com/example/FinancialSystem/dataProvider/adapter/AddressAdapter.java
@@ -0,0 +1,28 @@
+package com.example.FinancialSystem.dataProvider.adapter;
+
+import com.example.FinancialSystem.core.exception.Customer.CustomerZipCodeNotFoundException;
+import com.example.FinancialSystem.dataProvider.client.ViaCepClient;
+import com.example.FinancialSystem.dataProvider.dto.ViaCepResponse;
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class AddressAdapter {
+
+    private ViaCepClient client;
+
+    @Retry(name = "addressAdapterRetry", fallbackMethod = "getAddressFallBack")
+    public ViaCepResponse getAddress(String zipCode) {
+        return client.getViaCep(zipCode);
+    }
+
+    public ViaCepResponse getAddressFallBack(String zipCode) throws CustomerZipCodeNotFoundException {
+        log.error("Zipcode {} not found", zipCode);
+        throw new CustomerZipCodeNotFoundException(zipCode);
+    }
+}
+
